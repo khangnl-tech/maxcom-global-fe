3256c1424dfa49377ea6d3faf912d22d3b6f9f2f
khangnl <khangnl@khangnl.tech>
2024-06-21 04:01:43 +0700

    khangnl: sendmail

---
+++ js/sendMail.js
@@ -0,0 +1,18 @@
+(function(){
+    emailjs.init("3esSksjrGE6Mb4dYc");
+})();
+
+function sendEmail() {
+    emailjs.send("service_7nmjyxr", "template_pdrnimy", {
+        from_name: document.getElementById('from_name').value,
+        from_email: document.getElementById('from_email').value,
+        message: document.getElementById('from_phone').value
+    })
+    .then(function(response) {
+       console.log('SUCCESS!', response.status, response.text);
+       alert('Email sent successfully!');
+    }, function(error) {
+       console.log('FAILED...', error);
+       alert('Failed to send email. Please try again.');
+    });
+}
